746ba12a3fa49d842304ae88555cb31ce69cd1e9
yamagame <yamagame@mac.com>
2023-05-02 18:06:23 +0900

    err 処理

--- infra/repository/crud.go
+++ infra/repository/crud.go
@@ -10,24 +10,32 @@ type CRUDRepository[T any] struct {
 
 // Create implements CRUDRepository.Create
 func (s *CRUDRepository[T]) Create(object *T) (*T, error) {
-	s.DB.Create(object)
+	if err := s.DB.Create(object).Error; err != nil {
+		return nil, err
+	}
 	return object, nil
 }
 
 // Read implements CRUDRepository.Read
 func (s *CRUDRepository[T]) Read(object *T) (*T, error) {
-	s.DB.Take(object)
+	if err := s.DB.Take(object).Error; err != nil {
+		return nil, err
+	}
 	return object, nil
 }
 
 // Update implements CRUDRepository.Update
 func (s *CRUDRepository[T]) Update(object *T) (*T, error) {
-	s.DB.Updates(object)
+	if err := s.DB.Updates(object).Error; err != nil {
+		return nil, err
+	}
 	return object, nil
 }
 
 // Delete implements CRUDRepository.Delete
 func (s *CRUDRepository[T]) Delete(object *T) (*T, error) {
-	s.DB.Delete(object)
+	if err := s.DB.Delete(object).Error; err != nil {
+		return nil, err
+	}
 	return object, nil
 }
