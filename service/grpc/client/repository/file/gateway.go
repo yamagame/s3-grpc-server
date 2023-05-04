2bedd5d1b1fd78d60078f93740a656464c05a62f
yamagame <yamagame@mac.com>
2023-05-04 17:53:49 +0900

    gateway を移動

--- service/grpc/client/repository/file/gateway.go
+++ service/grpc/client/repository/file/gateway.go
@@ -2,9 +2,9 @@ package file
 
 import (
 	"sample/s3-grpc-server/infra/repository/model"
-	"sample/s3-grpc-server/infra/repository/table"
 	"sample/s3-grpc-server/libs/gateway"
 	"sample/s3-grpc-server/proto/grpc_server"
+	"sample/s3-grpc-server/service/grpc/gateway/table"
 )
 
 type CreateFile struct{}
